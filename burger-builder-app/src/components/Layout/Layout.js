5b60ee4020b12e48f38d98d7809122341f26d56b
hgonzaga <hugonzagamigl@gmail.com>
2020-04-06 19:06:31 +0100

    Navigation Toolbar (Logo+ CSS+ Navigation Items) + Side Drawer

--- burger-builder-app/src/components/Layout/Layout.js
+++ burger-builder-app/src/components/Layout/Layout.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 
-const layout = props => (
-  <Aux>
-    <div>Toolbar, SideDrawer, BackDrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+import Aux from "../../hoc/Auxiliary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-export default layout;
+class Layout extends Component {
+  state = { showSiderDrawer: true };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSiderDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSiderDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
