c762539c0555d9dbf71bb15656f4441aaa9016fe
hgonzaga <hugonzagamigl@gmail.com>
2020-09-03 22:29:29 +0100

    Section 11: Routing Assignment (Concluded)
    Exercise completion.

--- routing-assignment/src/containers/Course/Course.js
+++ routing-assignment/src/containers/Course/Course.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  state = {
+    courseTitle: "",
+  };
+
+  componentDidMount() {
+    this.parseQueryParams();
+  }
+
+  componentDidUpdate() {
+    this.parseQueryParams();
+  }
+
+  parseQueryParams() {
+    const query = new URLSearchParams(this.props.location.search);
+
+    for (let params of query.entries()) {
+      if (this.state.courseTitle !== params[1]) {
+        this.setState({ courseTitle: params[1] });
+      }
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.courseTitle}</h1>
+        <p>
+          You selected the Course with ID: {this.props.match.params.courseId}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Course;
